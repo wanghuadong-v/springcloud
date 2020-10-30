c8f91690ae2813712daf32395eedc2d2732c49a7
wanghuadong <1453240241@qq.com>
2020-10-30 09:09:45 +0800

    springcloud demo初始化

---
+++ cloud-provider-payment8001/src/main/java/com/demo/springboot/paymentMain8001.java
@@ -0,0 +1,9 @@
+package com.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class paymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(paymentMain8001.class, args);
+    }
+}
