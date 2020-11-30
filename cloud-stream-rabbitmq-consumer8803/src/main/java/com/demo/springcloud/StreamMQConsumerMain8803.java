ff4e2bf5bd27173c10efe565888f62eb290f06ec
wanghuadong <1453240241@qq.com>
2020-11-30 21:45:37 +0800

    stream-RabbitMQ

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/demo/springcloud/StreamMQConsumerMain8803.java
@@ -0,0 +1,16 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: wanghuadong
+ * @Date: 2020/11/29 16:17
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class StreamMQConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQConsumerMain8803.class,args);
+    }
+}
